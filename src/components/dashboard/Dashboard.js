8fc1f3cf9b65982d15e962a572a75aef294b60aa
nguyenthieuluan <trangkhuyet30@gmail.com>
2018-11-05 14:51:11 +0700

    add Pagination

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,30 +4,57 @@ import ProjectList from "../projects/ProjectList";
 import {connect} from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
+import Pagination from "react-js-pagination";
+import "./Dashboard.css";
 
 class Dashboard extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 1,
+      itemsCountPerPage: 10,
+    };
+  }
+
+  handlePageChange = (pageNumber) => {
+    //console.log(`active page is ${pageNumber}`);
+    this.setState({activePage: pageNumber});
+  }
+
   render() {
     const { projects, auth, notifications } = this.props;
-
+    const indexOfLastList = this.state.activePage * this.state.itemsCountPerPage;
+    const indexOfFirstList = indexOfLastList - this.state.itemsCountPerPage;
+    const projectsList = projects && projects.slice(indexOfFirstList, indexOfLastList);
     const notif = auth.uid ? <Notifications notifications={notifications}/> : null ;
+
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList projects={projects}/>
+            <ProjectList projects={projectsList}/>
           </div>
           <div className="col s12 m5 offset-m1">
             {notif}
           </div>
+        </div>
+        <Pagination
+          activePage={this.state.activePage}
+          itemsCountPerPage={this.state.itemsCountPerPage}
+          totalItemsCount={projects && projects.length}
+          pageRangeDisplayed={5}
+          onChange={this.handlePageChange}
+        />
+        <div className="white">
+          <p>Total threads: {projects && projects.length}</p>
         </div>
       </div>
     )
   }
 }
 const initMapStateToProps = (state) => {
-  //console.log(state)
   return {
-    projects: state.firestoreReducer.ordered.projects,
+    projects: state.firestoreReducer.ordered.projects, 
     auth: state.firebaseReducer.auth,
     notifications: state.firestoreReducer.ordered.notifications
   }
@@ -35,7 +62,7 @@ const initMapStateToProps = (state) => {
 export default compose(
   connect(initMapStateToProps),
   firestoreConnect([
-    {collection: 'projects', orderBy: ['createdAt', 'desc']},
+    {collection: 'projects',limit: 1000, orderBy: ['createdAt', 'desc']},
     {collection: 'notifications', limit: 5, orderBy: ['time', 'desc']}
   ])
 ) (Dashboard);
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,18 +1,49 @@
+function makeid(x) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkl mnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < x; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
 export const createProject = (project) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     const firestore = getFirestore();
     const profile = getState().firebaseReducer.profile;
     const authorId = getState().firebaseReducer.auth.uid;
-    firestore.collection('projects').add({
-      ...project,
-      authorFirstName: profile.firstName,
-      authorLastName: profile.lastName,
-      authorId: authorId,
-      createdAt: new Date()
-    }).then(()=>{
-      dispatch({ type: 'ADD_PROJECT', project})
-    }).catch((error)=>{
-      dispatch({ type: 'ADD_PROJECT_ERROR', error})
-    });
+    for (let index = 0; index < 200; index++) {
+      firestore.collection('projects').add({
+          //...project,
+          title: `${200 + index} ${makeid(10)}`,
+          content: makeid(1500),
+          authorFirstName: profile.firstName,
+          authorLastName: profile.lastName,
+          authorId: authorId,
+          createdAt: new Date()
+        }).catch((error)=>{
+          console.log(error)
+        });
+    }
   }
 };
+
+
+// export const createProject = (project) => {
+//   return (dispatch, getState, { getFirebase, getFirestore }) => {
+//     const firestore = getFirestore();
+//     const profile = getState().firebaseReducer.profile;
+//     const authorId = getState().firebaseReducer.auth.uid;
+//     firestore.collection('projects').add({
+//       ...project,
+//       authorFirstName: profile.firstName,
+//       authorLastName: profile.lastName,
+//       authorId: authorId,
+//       createdAt: new Date()
+//     }).then(()=>{
+//       dispatch({ type: 'ADD_PROJECT', project})
+//     }).catch((error)=>{
+//       dispatch({ type: 'ADD_PROJECT_ERROR', error})
+//     });
+//   }
+// };
