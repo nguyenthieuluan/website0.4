d6d3c1f0f60ef5fa613967622ebec4c539bd46d9
nguyenthieuluan <trangkhuyet30@gmail.com>
2018-11-09 14:12:32 +0700

    save

--- src/components/messenger/Messenger.js
+++ src/components/messenger/Messenger.js
@@ -19,25 +19,25 @@ export default class Messenger extends Component {
           <div className="col s12 m8 offset-m1">
           <div className="white">
          
-          <div className="container">
+          <div className="container-message">
             <img src="https://www.w3schools.com/w3images/bandmember.jpg" alt="Avatar"/>
             <p>Hello. How are you today?</p>
             <span className="time-right">11:00</span>
           </div>
 
-          <div className="container darker">
+          <div className="container-message darker">
             <img src="https://www.w3schools.com/w3images/avatar_g2.jpg" alt="Avatar" className="right"/>
             <p>Hey! I'm fine. Thanks for asking!</p>
             <span class="time-left">11:01</span>
           </div>
 
-          <div className="container">
+          <div className="container-message">
             <img src="https://www.w3schools.com/w3images/bandmember.jpg" alt="Avatar"/>
             <p>Sweet! So, what do you wanna do today?</p>
             <span className="time-right">11:02</span>
           </div>
 
-          <div className="container darker">
+          <div className="container-message darker">
             <img src="https://www.w3schools.com/w3images/avatar_g2.jpg" alt="Avatar" class="right"/>
             <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
             <span className="time-left">11:05</span>
